45749dfad16c1bd7bfad97e7b4ce3d1c127c12d7
zhuzhaoman <2358793988@qq.com>
2020-06-23 23:27:49 +0800

    1、将system注册到eureka 2、创建测试Controller

---
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.course.system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhuzhaoman
+ * @date 2020/6/23 0023 23:21
+ * @description 测试Controller
+ */
+
+@RestController
+@RequestMapping("/system")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "success";
+    }
+}
