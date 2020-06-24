192f13e2623755b1b5ed0fc4b15423ed19471d14
zhuzhaoman <2358793988@qq.com>
2020-06-24 13:24:00 +0800

    1、mybatis使用示例：http://127.0.0.1:8001/system/test

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.course.system.controller;
 
+import com.course.system.pojo.Test;
+import com.course.system.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author zhuzhaoman
@@ -13,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "success";
+    public List<Test> test() {
+        return testService.list();
     }
 }
---
+++ system/src/main/java/com/course/system/mapper/TestMapper.java
@@ -0,0 +1,15 @@
+package com.course.system.mapper;
+
+import com.course.system.pojo.Test;
+
+import java.util.List;
+
+/**
+ * @author zhuzhaoman
+ * @date 2020/6/24 0024 13:13
+ * @description 描述
+ */
+public interface TestMapper {
+
+    public List<Test> list();
+}
