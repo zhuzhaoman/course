dd30926ae321612659c97f61286f5058bd0f4b76
zhuzhaoman <2358793988@qq.com>
2020-06-26 20:28:09 +0800

    1、增加大章chapter表sql 2、生成持久层代码

--- doc/db/course.sql
+++ doc/db/course.sql
@@ -6,3 +6,11 @@ create table `test` (
 ) engine=innodb default charset=utf-8 comment='测试'
 
 insert into `test` (`id`, `name`) values (4, '赵六');
+
+drop table if exists `chapter`;
+create table `chapter` (
+    `id` varchar(255) not null comment 'ID',
+    `course_id` varchar(255) comment '课程ID',
+    `name` varchar(255) comment '名称',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
