43a3d5d0f7a56dbd67fee887729e7edc41642043
zhuzhaoman <2358793988@qq.com>
2020-06-28 23:22:43 +0800

    1、增加刷新功能

--- admin/src/views/service/chapter/index.vue
+++ admin/src/views/service/chapter/index.vue
@@ -1,30 +1,34 @@
 <template>
-  <el-table
-    :data="chapter"
-    border
-    style="width: 100%">
-    <el-table-column
-      prop="id"
-      label="ID">
-    </el-table-column>
-    <el-table-column
-      prop="courseId"
-      label="课程ID">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="大章名称">
-    </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作"
-      width="200">
-      <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-        <el-button type="text" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div class="container">
+    <el-button type="primary" icon="el-icon-refresh" @click="refresh()">刷新</el-button>
+    <el-table
+      :data="chapter"
+      border
+      v-loading="loading"
+      style="width: 100%">
+      <el-table-column
+        prop="id"
+        label="ID">
+      </el-table-column>
+      <el-table-column
+        prop="courseId"
+        label="课程ID">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="大章名称">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="200">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -33,7 +37,8 @@
     name: "index",
     data() {
       return {
-        chapter: []
+        chapter: [],
+        loading: true
       }
     },
     mounted() {
@@ -41,10 +46,15 @@
     },
     methods: {
       getList() {
+        this.loading = true
+
         list({page: 2, size: 1}).then(res => {
-          console.log(res)
           this.chapter = res.data.list
+          this.loading = false
         })
+      },
+      refresh() {
+        this.getList()
       }
     }
   }
