7d20faadf97bfefaf17aeae925a4cc3ed0e3a2dc
zhuzhaoman <2358793988@qq.com>
2020-07-01 21:04:54 +0800

    1、生成器开发：代码生成和mybatis generator整合

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -4,6 +4,11 @@ import com.course.generator.utils.DbUtil;
 import com.course.generator.utils.Field;
 import com.course.generator.utils.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -18,13 +23,35 @@ public class ServerGenerator {
     static String toServicePath = "server/src/main/java/com/course/server/service/";
     static String toDtoPath = "server/src/main/java/com/course/server/dto/";
     static String toControllerPath = MODULE + "/src/main/java/com/course/" + MODULE + "/controller/admin/";
+    static String generatorConfigPath = "server/src/main/resources/generator/generatorConfig.xml";
+
 
     public static void main(String[] args) throws Exception {
-        String Pojo = "Section";
-        String pojo = "section";
-        String tableNameCn = "小节";
+
         String module = MODULE;
-        List<Field> fieldList = DbUtil.getColumnByTableName(pojo);
+
+        // 只生成配置文件中的第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc=reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement=doc.getRootElement();
+        //读取context节点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个“table”的节点
+        tableElement=contextElement.elementIterator("table").next();
+        String Pojo = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String pojo = Pojo.substring(0, 1).toLowerCase() + Pojo.substring(1);
+        System.out.println("表："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
+
+
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaType(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Pojo", Pojo);
