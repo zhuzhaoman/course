e26a5501800346c9dabbef0d331096b9243d150f
zhuzhaoman <2358793988@qq.com>
2020-07-01 13:05:37 +0800

    1、小节表section表结构设计 2、生成section表持久层代码

--- doc/db/course.sql
+++ doc/db/course.sql
@@ -1,19 +1,19 @@
 drop table if exists `test`;
 create table `test` (
-	`id` int(12) not null default '' comment 'id',
-	`name` varchar(255) comment '名称',
-	primary key (`id`)
+    `id` int(12) not null default '' comment 'id',
+    `name` varchar(255) comment '名称',
+    primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='测试';
 
 insert into `test` (`id`, `name`) values (4, '赵六');
 
-
+-- 大章表
 drop table if exists `chapter`;
 create table `chapter` (
-    `id` varchar(255) not null comment 'ID',
-    `course_id` varchar(255) comment '课程ID',
-    `name` varchar(255) comment '名称',
-    primary key (`id`)
+   `id` varchar(255) not null comment 'ID',
+   `course_id` varchar(255) comment '课程ID',
+   `name` varchar(255) comment '名称',
+   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='大章';
 
 insert into `chapter` (id, course_id, name) values('0001', '0001', '测试大章01');
@@ -38,3 +38,24 @@ insert into `chapter` (id, course_id, name) values('0019', '0019', '测试大章
 insert into `chapter` (id, course_id, name) values('0020', '0020', '测试大章20');
 insert into `chapter` (id, course_id, name) values('0021', '0021', '测试大章21');
 insert into `chapter` (id, course_id, name) values('0022', '0022', '测试大章22');
+
+
+-- 小节表
+drop table if exists `section`;
+create table `section` (
+   `id` varchar(48) not null default '' comment 'id',
+   `title` varchar(50) not null comment '标题',
+   `course_id` varchar(48) comment '课程|course.id',
+   `chapter_id` varchar(48) comment '大章|chapter.id',
+   `video` varchar(200) comment '视频',
+   `time` int comment '时长|单位秒',
+   `charge` char(1) comment '收费|C 收费；F 免费',
+   `sort` int comment '顺序',
+   `created_at` datetime(3) comment '创建时间',
+   `updated_at` datetime(3) comment '修改时间',
+   primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='小节';
+
+
+insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
+values ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'f', 1, now(), now());
